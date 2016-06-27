c891b8cb2ed5770d4d2c1ad71805b4b31abc53a5
andrewbihl <andrewbihlva@gmail.com>
2016-06-27 18:20:08 -0500

    Cleaned up small interface details, made target number completely random in range 100-999

--- CountWithFriends/GameBoardViewController.swift
+++ CountWithFriends/GameBoardViewController.swift
@@ -16,7 +16,7 @@ class GameBoardViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        startNewRound()
         // Do any additional setup after loading the view.
     }
     
