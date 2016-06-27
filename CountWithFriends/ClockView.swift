14c68c3dad6361c1cccb10b5a6e97c88ab29eeaa
Paul Lefebvre <paullefebvre@Pauls-MacBook-Pro.local>
2016-06-27 18:21:33 -0500

    Add clocktimer uiview

---
+++ CountWithFriends/ClockView.swift
@@ -0,0 +1,21 @@
+//
+//  ClockView.swift
+//  CountWithFriends
+//
+//  Created by Paul Lefebvre on 6/27/16.
+//  Copyright © 2016 Andrew Bihl. All rights reserved.
+//
+
+import UIKit
+
+class ClockView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
